037d96d7fc46494258f64625e299ecc6d65885f7
Daniel Hernandez <dh893177@gmail.com>
2024-05-03 15:33:50 -0500

    setup html page

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,16 @@
 "use strict"
+
+window.onload = function(){
+    let iceCreamOrder = document.querySelector("#iceCreamOrder")
+
+    iceCreamOrder.addEventListener("submit, calcOrder")
+
+}
+
+function calcOrder(event){
+    event.preventDefault();
+
+    let theOrder = event.target
+
+    let totalForOrder = 2.25 * Number(theOrder.numScoops.value)
+}
